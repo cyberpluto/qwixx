48104ae4470b20f6b9015a1a71fa94fee86c2dd9
arek <arkadiusz.zalewsky@gmail.com>
2016-09-11 12:49:00 +0200

    Added Webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,12 @@
+const webpack = require("webpack")
+
 module.exports = {
   devtool: 'source-map',
-  entry: "./app/Qwixx.jsx",
+  entry: [
+    'webpack-dev-server/client?http://0.0.0.0:8080',
+    'webpack/hot/only-dev-server',
+    './app/Qwixx.jsx',
+  ],
   output: {
     filename: "bundle.js",
     path: __dirname + "",
@@ -10,20 +16,23 @@ module.exports = {
       {
         test: /\.jsx?$/,
         exclude: /(node_modules|bower_components)/,
-        loader: 'babel',
-        query: {
-          plugins: ['transform-decorators-legacy'],
-          presets: ['react', 'es2015', 'stage-2']
-        }
+        loaders: ['react-hot', 'babel'],
+        loaders: [
+          // 'react-hot', 
+          'babel?presets[]=es2015,presets[]=stage-2,presets[]=react,plugins[]=transform-decorators-legacy,plugins[]=react-hot-loader/babel'
+        ]
       },
       {
         test: /\.(css|scss)$/,
         loaders: [
-            'style',
-            'css?sourceMap&modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]',
-            'sass'
+          'style',
+          'css?sourceMap&modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]',
+          'sass'
         ]
       }
     ]
-  }
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ]
 };
