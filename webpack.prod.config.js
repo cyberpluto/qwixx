a8327f73ef48ef87ed7a6e08caea593132b48588
arek <arkadiusz.zalewsky@gmail.com>
2016-09-24 22:56:49 +0200

    Created separate webpack.config file for production.

---
+++ webpack.prod.config.js
@@ -0,0 +1,34 @@
+const webpack = require("webpack")
+
+module.exports = {
+  devtool: 'source-map',
+  entry: './app/Qwixx.jsx',
+  output: {
+    filename: "bundle.js",
+    path: __dirname + "/build/",
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loaders: ['react-hot', 'babel'],
+        loaders: [
+          // 'react-hot', 
+          'babel?presets[]=es2015,presets[]=stage-2,presets[]=react,plugins[]=transform-decorators-legacy,plugins[]=react-hot-loader/babel'
+        ]
+      },
+      {
+        test: /\.(css|scss)$/,
+        loaders: [
+          'style',
+          'css?sourceMap&modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]',
+          'sass'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ]
+};
